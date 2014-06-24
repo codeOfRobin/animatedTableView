5ed0de94e2e0b5ef126ff1b38ddcd24c2ce572f0
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-06-25 01:12:26 +0530

    Finished with the expansion thing. This could make a pretty awesome open source library

--- animatedTableView/pinchToExpandTableViewController.m
+++ animatedTableView/pinchToExpandTableViewController.m
@@ -9,9 +9,15 @@
 #import "pinchToExpandTableViewController.h"
 
 @interface pinchToExpandTableViewController ()
-
+@property (nonatomic) CGFloat initialPinchHeight;
+@property (nonatomic) NSIndexPath *pinchedIndexPath;
+@property (nonatomic) NSInteger uniformRowHeight;
 @end
 
+#pragma mark -
+
+#define DEFAULT_ROW_HEIGHT 88
+#define HEADER_HEIGHT 48
 @implementation pinchToExpandTableViewController
 
 - (id)initWithStyle:(UITableViewStyle)style
@@ -26,6 +32,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    
+    UIPinchGestureRecognizer *pinchRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinch:)];
+	[self.tableView addGestureRecognizer:pinchRecognizer];
+    
+    // Set up default values.
+    self.tableView.sectionHeaderHeight = HEADER_HEIGHT;
+	/*
+     The section info array is thrown away in viewWillUnload, so it's OK to set the default values here. If you keep the section information etc. then set the default values in the designated initializer.
+     */
+    self.uniformRowHeight = DEFAULT_ROW_HEIGHT;
+
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -46,26 +64,93 @@
 {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return 6;
+}
+#pragma mark - Handling pinches
+
+- (void)handlePinch:(UIPinchGestureRecognizer *)pinchRecognizer {
+    
+    /*
+     There are different actions to take for the different states of the gesture recognizer.
+     * In the Began state, use the pinch location to find the index path of the row with which the pinch is associated, and keep a reference to that in pinchedIndexPath. Then get the current height of that row, and store as the initial pinch height. Finally, update the scale for the pinched row.
+     * In the Changed state, update the scale for the pinched row (identified by pinchedIndexPath).
+     * In the Ended or Canceled state, set the pinchedIndexPath property to nil.
+     */
+    
+    if (pinchRecognizer.state == UIGestureRecognizerStateBegan) {
+        
+        CGPoint pinchLocation = [pinchRecognizer locationInView:self.tableView];
+        NSIndexPath *newPinchedIndexPath = [self.tableView indexPathForRowAtPoint:pinchLocation];
+		self.pinchedIndexPath = newPinchedIndexPath;
+        
+        // Alternatively, set initialPinchHeight = uniformRowHeight.
+        self.initialPinchHeight=self.uniformRowHeight;
+        [self updateForPinchScale:pinchRecognizer.scale atIndexPath:newPinchedIndexPath];
+    }
+    else {
+        if (pinchRecognizer.state == UIGestureRecognizerStateChanged) {
+            [self updateForPinchScale:pinchRecognizer.scale atIndexPath:self.pinchedIndexPath];
+        }
+        else if ((pinchRecognizer.state == UIGestureRecognizerStateCancelled) || (pinchRecognizer.state == UIGestureRecognizerStateEnded)) {
+            self.pinchedIndexPath = nil;
+        }
+    }
 }
 
-/*
+- (void)updateForPinchScale:(CGFloat)scale atIndexPath:(NSIndexPath *)indexPath {
+    
+    if (indexPath && (indexPath.section != NSNotFound) && (indexPath.row != NSNotFound)) {
+        
+		CGFloat newHeight = round(MAX(self.initialPinchHeight * scale, DEFAULT_ROW_HEIGHT));
+        
+        self.uniformRowHeight=newHeight;
+        
+        /*
+         Switch off animations during the row height resize, otherwise there is a lag before the user's action is seen.
+         */
+        BOOL animationsEnabled = [UIView areAnimationsEnabled];
+        [UIView setAnimationsEnabled:NO];
+        [self.tableView beginUpdates];
+        [self.tableView endUpdates];
+        [UIView setAnimationsEnabled:animationsEnabled];
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"%ld",(long)indexPath.row);
+    NSLog(@"%ld",(long)self.pinchedIndexPath.row);
+    if (indexPath.row==self.pinchedIndexPath.row)
+    {
+        return self.uniformRowHeight;
+
+    }
+    else
+    {
+        return DEFAULT_ROW_HEIGHT;
+    }
+    // Alternatively, return rowHeight.
+}
+
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    UITextView *text=(UITextView *)[cell viewWithTag:1];
+    text.text=@"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.";
     
     // Configure the cell...
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
